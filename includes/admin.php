983140a2be1fefe105d82f8527950a88f644d039
Henrique Moraes <chmoraes9@gmail.com>
2022-04-06 16:15:33 -0300

    update

--- includes/admin.php
+++ includes/admin.php
@@ -1,12 +1,5 @@
 <div class="wrap">
-    <style>
-        #administrar{
-            background: yellow;
-        }
-        .notice{
-            display:none;
-        }
-    </style>
+
 <div id="administrar">
     <header>
         <h2>Administração de pedidos</h2>
@@ -14,7 +7,4 @@
 <?php include 'incluir_status.php'; ?>
 
 </div>
-<script>
-
-</script>
 </div>
